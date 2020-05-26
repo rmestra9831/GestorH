dbc453a5f3e84c21ebd391b68bbc3d82a929e702
richard mestra <richardmestra9831@gmail.com>
2020-05-26 13:43:42 -0500

    Inicio

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="https://creative-tim.com/presentation">
+                {{ __('Sobre Nosotros') }}
+            </a>
+            </li>
+            <li>
+            <a href="https://www.creative-tim.com/license">
+                {{ __('Licenses') }}
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>,Made By <strong>Grupo 3</strong>
+        </div>
+    </div>
+</footer>
